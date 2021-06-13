6ba3f3de54adfbad84169be55f3a7d79b0ba655e
zoro <dilip@home.com>
2021-06-13 20:57:10 +0530

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+const bodyOverlay = document.getElementById("body-overlay");
+
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+      toggle.classList.toggle("bx-x");
+      bodyOverlay.classList.toggle("active");
+    });
+  }
+};
+showMenu("header-toggle", "nav-menu");
+
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  navLink.forEach((n) => n.classList.remove("active"));
+  this.classList.add("active");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
